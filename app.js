557993ff9809e8bd5df2bf6571bf980b917e0b3b
hangju0610 <phj950610@naver.com>
2023-07-15 16:27:54 +0900

    CI 환경 구축

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 // const cookieParser = require("cookie-parser");
 const app = express();
-const indexRouter = require("./routes/indexRoutes.js");
-const env = require('dotenv')
-env.config()
+const indexRouter = require('./routes/indexRoutes.js');
+const env = require('dotenv');
+env.config();
 
-
-app.use(express.json()) 
+app.use(express.json());
 // app.use(cookieParser())
-app.use("/api", indexRouter) 
+app.use('/api', indexRouter);
 
-
-app.listen(process.env.PORT, () => {
-    console.log(process.env.PORT, '포트로 서버가 열렸어요!');
+app.get('/', (req, res) => {
+  console.log('CICD test');
 });
 
-
+app.listen(process.env.PORT, () => {
+  console.log(process.env.PORT, '포트로 서버가 열렸어요!');
+});
---
+++ scripts/after_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+REPOSITORY=/home/ubuntu/hh99_miniProject
+
+cd $REPOSITORY
+
+sudo npm ci
+
+npm start
