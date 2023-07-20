74ddf06fdaeb0b200d928595377a0932ce0e9420
hangju0610 <phj950610@naver.com>
2023-07-20 14:34:02 +0900

    refac: sequelize config 파일 편집

--- config/config.js
+++ config/config.js
@@ -1,25 +1,30 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const development = {
   username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   host: process.env.MYSQL_HOST,
-  dialect: "mysql",
+  dialect: 'mysql',
+  dialectOptions: {
+    charset: 'utf8mb4',
+    dateStrings: true,
+    typeCast: true,
+  },
 };
 const test = {
-  username: "root",
+  username: 'root',
   password: null,
-  database: "database_test",
-  host: "127.0.0.1",
-  dialect: "mysql",
+  database: 'database_test',
+  host: '127.0.0.1',
+  dialect: 'mysql',
 };
 const production = {
-  username: "root",
+  username: 'root',
   password: null,
-  database: "database_production",
-  host: "127.0.0.1",
-  dialect: "mysql",
+  database: 'database_production',
+  host: '127.0.0.1',
+  dialect: 'mysql',
 };
 
 module.exports = { development, test, production };
